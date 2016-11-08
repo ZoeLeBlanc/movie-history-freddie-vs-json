bebc617927291b2245257f5cf7ba8c88501012d5
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-11-07 21:11:49 -0600

    login etc

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,24 @@
 
 let getMovie = require("./getMovie");
 let credentials = require("./credentials");
+let firebaseUser = require("./firebaseUser");
+let firebaseAuth = require("./firebaseAuth");
+// let getUser = require("./firebaseUser");
+// let loginUser = require("./firebaseAuth");
+// let logoutUser = require("./firebaseAuth");
+
 let apiKeys = {};
+let uid = "";
+
+
+function createLogoutButton(){
+	firebaseUser.getUser(apiKeys, uid).then(function(userResponse){
+		$("#logout-container").html("");
+		let currentUsername = userResponse.username;
+		let logoutButton = `<button class="btn btn-danger" id="logoutButton">LOGOUT ${currentUsername}</button>`;
+		$("#logout-container").append(logoutButton);
+	});
+}
 
 $(document).ready(function() {
 	credentials().then( (keys)=>{
@@ -13,4 +30,64 @@ $(document).ready(function() {
 	getMovie("Top Gun").then((returnedMovie)=>{
 		console.log("returned Movie: ", returnedMovie);
 	});
+
+	$("#registerButton").on("click", function(){
+		let email = $("#inputEmail").val();
+		let password = $("#inputPassword").val();
+		let username = $("#inputUsername").val();
+
+		let user = {
+			"email": email,
+			"password": password
+		};
+
+		firebaseAuth.registerUser(user).then(function(registerResponse){
+			console.log("register response", registerResponse);
+			let newUser = {
+				"username": username,
+				"uid": registerResponse.uid
+			};
+			
+			return firebaseUser.addUser(apiKeys, newUser);
+			//return FbAPI.loginUser(user);
+
+		}).then(function(addUserResponse){
+			return firebaseAuth.loginUser(user);
+		}).then(function(loginResponse){
+			console.log("login response", loginResponse);
+			uid = loginResponse.uid;
+			createLogoutButton();
+			$("#login-container").addClass("hide");
+			$("#list-container").removeClass("hide");
+		});
+	});
+
+	$("#loginButton").on("click", function(){
+		let email = $("#inputEmail").val();
+		let password = $("#inputPassword").val();
+
+		let user = {
+			"email": email,
+			"password": password
+		};
+
+		firebaseAuth.loginUser(user).then(function(loginResponse){
+			uid = loginResponse.uid;
+			createLogoutButton();
+			$("#login-container").addClass("hide");
+			$("#movie-container").removeClass("hide");
+		});
+	});
+
+	$("#logout-container").on("click", "#logoutButton", function(){
+		firebaseAuth.logoutUser();
+		uid="";
+		
+		$("#inputEmail").val("");
+		$("#inputPassword").val("");
+		$("#inputUsername").val("");
+		$("#login-container").removeClass("hide");
+		$("#movie-container").addClass("hide");
+	});
+
 });
